dba7ea7044b266ee5f077569028b49b9552a964f
hou-12 <82287928+hou-12@users.noreply.github.com>
2023-05-12 14:57:45 +0200

    Update __init__.py

--- inversion-list/__init__.py
+++ inversion-list/__init__.py
@@ -1,8 +1,8 @@
 """
-The arithmetic module.
+The inversion-list module.
 """
 
 # pylint: disable=no-name-in-module
-from ._arithmetic import gcd, lcm
+from ._inversion-list import gcd, lcm
 
 __all__ = ("gcd", "lcm")
