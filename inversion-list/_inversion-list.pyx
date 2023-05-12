14df9d5f55bfe6a7629c901c80cee872ea5236f0
hou-12 <82287928+hou-12@users.noreply.github.com>
2023-05-12 06:44:30 +0200

    Update _inversion-list.pyx

--- inversion-list/_inversion-list.pyx
+++ inversion-list/_inversion-list.pyx
@@ -1,5 +1,5 @@
-# distutils: sources = inversion-list/src/inversion-list/inversion-list.c 
-# distutils: include_dirs = inversion-list/inversion-list/src/inversion-list
+# distutils: sources = inversion-list/c-inversion-list/src/inversion-list/inversion-list.c 
+# distutils: include_dirs = inversion-list/c-inversion-list/src/inversion-list
 # distutils: extra_compile_args = -O3
 
 cimport inversion-list.c_inversion-list
