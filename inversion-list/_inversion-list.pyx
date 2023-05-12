d633eb4e0cc9301385eb1f8df5e73e6f4ab55cda
hou-12 <82287928+hou-12@users.noreply.github.com>
2023-05-12 06:23:36 +0200

    Update _inversion-list.pyx

--- inversion-list/_inversion-list.pyx
+++ inversion-list/_inversion-list.pyx
@@ -2,41 +2,7 @@
 # distutils: include_dirs = arithmetic/c-arithmetic/src/numeric
 # distutils: extra_compile_args = -O3
 
-cimport arithmetic.c_arithmetic
+cimport inversion-list.c_inversion-list
 
-def gcd(a: int, b: int) -> int:
-    """
-    Compute the greatest common divisor of two numbers.
-    
-    Parameters
-    ----------
-    a : int
-        The first number
-    b : int
-        The second number
 
-    Returns
-    -------
-    int
-        The greatest common divisor.         
-    """
-    return arithmetic.c_arithmetic.arithmetic_gcd(a, b)
-    
-def lcm(a: int, b: int) -> int:
-    """
-    Compute the least common multiple of two numbers.
-    
-    Parameters
-    ----------
-    a : int
-        The first number
-    b : int
-        The second number
-
-    Returns
-    -------
-    int
-        The least common multiple.         
-    """
-    return arithmetic.c_arithmetic.arithmetic_lcm(a, b)
     
