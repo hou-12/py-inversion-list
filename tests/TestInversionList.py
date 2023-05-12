2d2603bc97801a1e95a411d4d6ae2a25e99e95dd
hou-12 <82287928+hou-12@users.noreply.github.com>
2023-05-12 16:41:12 +0200

    Update TestInversionList.py

--- tests/TestInversionList.py
+++ tests/TestInversionList.py
@@ -3,7 +3,6 @@ from unittest import TestCase
 from inversionList import IntegerSet
 
 
-class InversionListTestCase(TestCase):
-    def test_create(self):
-        self.assertEqual(0, 0)
+
+    
         
