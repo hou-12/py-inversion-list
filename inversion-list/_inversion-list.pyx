635a3714460708b53d0e10c815e8334735c54aa5
hou-12 <82287928+hou-12@users.noreply.github.com>
2023-05-12 15:33:03 +0200

    Update _inversion-list.pyx

--- inversion-list/_inversion-list.pyx
+++ inversion-list/_inversion-list.pyx
@@ -1,8 +1,13 @@
 # distutils: sources = inversion-list/c-inversion-list/src/inversion-list/inversion-list.c 
 # distutils: include_dirs = inversion-list/c-inversion-list/src/inversion-list
 # distutils: extra_compile_args = -O3
+from typing import AbstractSet, Iterable, Iterator, Optional, Tuple
 
 cimport inversion-list.c_inversion-list
 class IntegerSet(AbstractSet[int]):
     def __init__(self, intervals: Optional[Iterable[Tuple[int, int]]] = None,) -> None: 
-        
+        if intervals is not None:
+            inversion_list = inversion_list_create(intervals)
+            self.intervals = intervals
+        else:
+            self.intervals = []
