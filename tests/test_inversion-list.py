6c53549f68b94a84d71bb19f0891a9988592afea
hou-12 <82287928+hou-12@users.noreply.github.com>
2023-05-12 15:39:44 +0200

    Update and rename test_arithmetic.py to test_inversion-list.py

---
+++ tests/test_inversion-list.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+from inversionList import IntegerSet
+
+
+class InversionListTestCase(TestCase):
+    def test_create(self):
+        self.assertEqual(0, 0)
+        
